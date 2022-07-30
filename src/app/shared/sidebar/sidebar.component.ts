d7f75703175d93242a3cecd631ef8bd37a91d0e5
HectorARG <hectorr9577@gmail.com>
2022-07-29 20:48:38 -0500

    Creacion de servicios insersion y almacenamiento de busqueda

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+
+  HistorialBusqueda: string[] = [];
+
+  constructor(private gifsService: GifsService) { }
+
+
+  get historial(): string[]{    
+    return this.gifsService.historial
+  }
+
+}
